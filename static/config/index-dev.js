a3b6f2109a753f6147e8a5d455940ae8a3b5e202
luoyusoft <luoyusoft@126.com>
2020-08-15 00:41:22 +0800

    1、修改请求url

--- static/config/index-dev.js
+++ static/config/index-dev.js
@@ -4,5 +4,5 @@
 ;(function () {
   window.SITE_CONFIG = {}
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://121.36.44.234:9999/luoyublog'
+  window.SITE_CONFIG['baseUrl'] = 'https://121.36.44.234:8443/luoyublog'
 })()
