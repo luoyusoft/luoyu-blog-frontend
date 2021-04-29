4a5e8db57ef848098c62a9d37f1b70885031a682
jinhaoxun <jinhaoxun@zhuanzhuan.comssh-add>
2021-04-29 11:49:30 +0800

    1. 修改域名

--- static/config/index-prod.js
+++ static/config/index-prod.js
@@ -4,7 +4,7 @@
 ;(function () {
   window.SITE_CONFIG = {}
   // api接口请求地址
-  window.SITE_CONFIG['baseApiUrl'] = 'https://luoyublog.com/api/luoyublog'
-  window.SITE_CONFIG['baseUrl'] = 'https://luoyublog.com'
-  window.SITE_CONFIG['baseWSUrl'] = 'wss://luoyublog.com/api/chat/'
+  window.SITE_CONFIG['baseApiUrl'] = 'https://jinhx.cc/api/luoyublog'
+  window.SITE_CONFIG['baseUrl'] = 'https://jinhx.cc'
+  window.SITE_CONFIG['baseWSUrl'] = 'wss://jinhx.cc/api/chat/'
 })()
